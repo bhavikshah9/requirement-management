38e8f91f03e0bf6dc77abef4f202aa95df03ee70
Bhavik Shah <bhavikshah835@gmail.com>
2019-12-24 18:20:06 +0530

    Updated version

--- nr.bat
+++ nr.bat
@@ -1,8 +1,10 @@
 @echo off
 cls
 setlocal enabledelayedexpansion
-set requirement_base_directory=F:\repo\office\inqueries
-set current_year=20%date:~7,4%
+
+set requirement_base_directory=<Path where you want to store all requirements>
+set files_dir=<Path where you have saved files with BDEs and Channels>
+set current_year=%date:~6,4%
 IF NOT EXIST %requirement_base_directory%\%current_year% (
   mkdir %requirement_base_directory%\%current_year%
 )
@@ -16,17 +18,13 @@ if %1 == y (
 )
 
 :benchmark1
-set current_month_partial=%date:~3,3%
-set nbr=0
-for %%1 in (Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec) do (
-  set /a nbr+=1
-  echo %current_month_partial%|findstr /i /b "%%1" >nul&& goto finis
-)
-
+set month-num=%date:~3,2%
+IF "%month-num:~0,1%"=="0" SET month-num=%month-num:~1%
+FOR /f "tokens=%month-num%" %%a in ("jan feb mar apr may jun jul aug sep oct nov dec") do set mo-name=%%a
 :finis
-if %nbr% lss 10 set nbr=0%nbr%
+if %month-num% lss 10 set month-num=0%month-num%
 
-set current_month=%nbr%-%current_month_partial%
+set current_month=%month-num%-%mo-name%
 IF NOT EXIST %requirement_base_directory%\%current_year%\%current_month% (
   mkdir %requirement_base_directory%\%current_year%\%current_month%
 )
@@ -43,8 +41,30 @@ if %1 == m (
 set /p requirement_name=Enter Requirement name: 
 IF NOT EXIST %requirement_base_directory%\%current_year%\%current_month%\%requirement_name% (
   mkdir %requirement_base_directory%\%current_year%\%current_month%\%requirement_name%
+) ELSE (
+	ECHO ERROR: Directory already exists.
+	GOTO exit
 )
 set custom_path=%requirement_base_directory%\%current_year%\%current_month%\%requirement_name%
+echo Requirement on %date%>> %custom_path%\metadata.txt
+
+for /f "delims=. tokens=1,2" %%G in (%files_dir%\bdes.txt) do (
+	set bde[%%G]=%%H
+	call echo %%G. %%H
+)
+set /p requirement_sender=Requirement sent by: 
+
+echo Requirement from !bde[%requirement_sender%]!>> %custom_path%\metadata.txt
+
+for /f "delims=. tokens=1,2" %%J in (%files_dir%\channels.txt) do (
+	set sharing_channel[%%J]=%%K
+	call echo %%J. %%K
+)
+set /p channel_used=Shared via: 
+
+echo Shared via !sharing_channel[%channel_used%]!>> %custom_path%\metadata.txt
 
 :open
 %SystemRoot%\explorer.exe %custom_path%
+
+:exit
